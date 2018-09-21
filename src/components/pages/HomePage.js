ac0dc2f3c27e333bafc5c17e5873a03b63ca3027
sefa-sahinoglu <sefa.sahinoglu@reateknoloji.com>
2018-09-22 00:32:01 +0300

    Login and Logout

--- src/components/pages/HomePage.js
+++ src/components/pages/HomePage.js
@@ -1,12 +1,27 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
-const HomePage = () => {
+import { connect } from 'react-redux';
+import PropTypes from 'prop-types';
+import { Button } from 'semantic-ui-react';
+import * as actions from '../../actions/auth';
+
+const HomePage = ({ isAuthenticated, logout }) => {
 	return (
 		<div>
 			<h1>Home Page</h1>
-			<Link to="/login">Login</Link>
+			{isAuthenticated ? <Button onClick={() => logout()}>Logout</Button> : <Link to="/login">Login</Link>}
 		</div>
 	);
 };
+HomePage.propTypes = {
+	isAuthenticated: PropTypes.bool.isRequired,
+	logout: PropTypes.func.isRequired
+};
 
-export default HomePage;
+function mapStateToProps(state) {
+	return {
+		isAuthenticated: !!state.user.token
+	};
+}
+
+export default connect(mapStateToProps, { logout: actions.logout })(HomePage);
--- src/index.js
+++ src/index.js
@@ -8,8 +8,14 @@ import { Provider } from 'react-redux';
 import thunk from 'redux-thunk';
 import rootReducer from './rootReducer';
 import { composeWithDevTools } from 'redux-devtools-extension';
+import { userLoggedIn } from './actions/auth';
 
 const store = createStore(rootReducer, composeWithDevTools(applyMiddleware(thunk)));
+
+if (localStorage.exampleJWT) {
+	const user = { token: localStorage.exampleJWT };
+	store.dispatch(userLoggedIn(user));
+}
 
 ReactDOM.render(
 	<BrowserRouter>
