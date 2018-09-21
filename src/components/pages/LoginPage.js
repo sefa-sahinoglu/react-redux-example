be094f91bb22ce78de799ea6a0b015445c126bbf
sefa-sahinoglu <sefa.sahinoglu@reateknoloji.com>
2018-09-21 20:15:35 +0300

    Login form with client-side validations

--- src/components/pages/LoginPage.js
+++ src/components/pages/LoginPage.js
@@ -1,11 +1,18 @@
 import React from 'react';
+import LoginForm from '../forms/LoginForm';
 
-const LoginPage = () => {
-	return (
-		<div>
-			<h1>Login</h1>
-		</div>
-	);
-};
+class LoginPage extends React.Component {
+	submit = (data) => {
+		console.log(data);
+	};
+	render() {
+		return (
+			<div>
+				<h1>Login</h1>
+				<LoginForm submit={this.submit} />
+			</div>
+		);
+	}
+}
 
 export default LoginPage;
